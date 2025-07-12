28910c8ebda30fc1744cc69a13f1f361acaa2655
1Huy2409 <nhathuy2409@gmail.com>
2025-07-12 15:48:20 +0700

    validation auth dto

--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,4 +1,8 @@
-export interface LoginDTO {
+import { IsNotEmpty } from 'class-validator'
+export class LoginDTO {
+    @IsNotEmpty({ message: "Email cant be empty!" })
     username: string;
+
+    @IsNotEmpty({ message: "Password cant be empty" })
     password: string
 }
--- src/modules/auth/dto/register.dto.ts
+++ src/modules/auth/dto/register.dto.ts
@@ -1,6 +1,17 @@
-export interface RegisterDTO {
+import { IsEmail, IsNotEmpty, MinLength, IsString, Min } from "class-validator";
+export class RegisterDTO {
+    @IsNotEmpty({ message: "Fullname cant be empty!" })
     fullname: string;
+
+    @IsNotEmpty({ message: "Username cant be empty!" })
+    @MinLength(5, { message: "Username must be at least 5 characters!" })
     username: string;
+
+    @IsNotEmpty({ message: "Email cant be empty!" })
+    @IsEmail()
     email: string;
-    password: string;   
+
+    @IsString({ message: "Password must be string!" })
+    @MinLength(6, { message: "Password must be at least 6 characters!" })
+    password: string;
 }
